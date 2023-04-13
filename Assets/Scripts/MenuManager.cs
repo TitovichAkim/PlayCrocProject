918bf438c7c061017a6d3a1492d57c6aca5f27aa
TitovichAkim <vas_pisar@mail.ru>
2023-04-13 15:24:12 +0300

    Update MenuManager.cs

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -33,7 +33,7 @@ public class MenuManager : MonoBehaviour
     }
     public void CollectTheReward ()
     {
-        timerText.text = $"You were not in the game: {$"{(int)currentTime.TotalHours}:{currentTime.TotalMinutes % 59:00}:{currentTime.TotalSeconds % 59:00}"}";
+        timerText.text = $"You were not in the game: {$"{(int)currentTime.TotalHours}:{currentTime.TotalMinutes % 60:00}:{currentTime.TotalSeconds % 60:00}"}";
 
 
         foreach (ProductPanelManager productPanelManager in shopManager.productPanelsArray)
